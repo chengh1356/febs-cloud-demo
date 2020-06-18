f99aee1768c4d63ccbf9f12e12e9cf0f00c55f34
cgh <540458161@qq.com>
2020-06-18 17:18:06 +0800

    febs-server

---
+++ febs-server/febs-server-test/src/main/java/cc/mrbird/febs/server/test/controller/TestController.java
@@ -0,0 +1,33 @@
+package cc.mrbird.febs.server.test.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+/**
+ * @Description
+ * @Author cgh
+ * @Date 2020-06-18 下午 5:13
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("test1")
+    @PreAuthorize("hasAnyAuthority('user:add')")
+    public String test1(){
+        return "拥有'user:add'权限";
+    }
+
+    @GetMapping("test2")
+    @PreAuthorize("hasAnyAuthority('user:update')")
+    public String test2(){
+        return "拥有'user:update'权限";
+    }
+
+    @GetMapping("user")
+    public Principal currentUser(Principal principal) {
+        return principal;
+    }
+}
